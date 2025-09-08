774f4d9f60b5e495fd369be51c50975d7466beb9
tai nguyen <ntai11206@gmail.com>
2025-09-08 15:50:52 +0700

    update config

--- lms/src/main/java/com/ntt/lms/config/WebSecurityConfig.java
+++ lms/src/main/java/com/ntt/lms/config/WebSecurityConfig.java
@@ -66,7 +66,10 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://ielts-lms-frontend.onrender.com"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
@@ -76,5 +79,6 @@ public class WebSecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+
 
 }
